60e0154d743cac28bd90452b3b05a09bbbed45f0
Yingze Wang <ericwangyz@berkeley.edu>
2024-12-21 01:37:27 -0800

    rewrite constrained embedding

--- easybfe/docking/embed.py
+++ easybfe/docking/embed.py
@@ -127,23 +127,65 @@ def constrained_optimization(mol: Chem.Mol, protein_pdb: os.PathLike, coord_map:
     ligand_pos = mol.GetConformer(0).GetPositions() / 10
     num_ligand_atoms = ligand_top.getNumAtoms()
 
-    if protein_pdb:
-        pdb = app.PDBFile(str(protein_pdb))
-        modeller = app.Modeller(pdb.topology, pdb.positions)
-        modeller.add(ligand_top, ndarray2quantity(ligand_pos))
-        top, pos = modeller.getTopology(), modeller.getPositions()
-        ff = app.ForceField('amber14/protein.ff14SB.xml', 'amber14/tip3p.xml')
-        pos = np.array([[vec.x, vec.y, vec.z] for vec in pos])
-        # coord map + #protein atoms offsets
-        new_coord_map = {}
-        for key, value in coord_map.items():
-            new_coord_map[key + pdb.topology.getNumAtoms()] = value
-        coord_map = new_coord_map
+    # Ligand only, no protein
+    ligand_ff = app.ForceField()
+    ligand_ff.registerTemplateGenerator(generator)
+    system = ligand_ff.createSystem(
+        ligand_top, 
+        nonbondedMethod=app.CutoffNonPeriodic, nonbondedCutoff=1.0*unit.nanometer, 
+        constraints=None, rigidWater=False, removeCMMotion=False
+    )
+
+    constrIndices = []
+    for atom in ligand_top.atoms():
+        if atom.index in coord_map and atom.element.symbol != 'H':
+            constrIndices.append(atom.index)
+    if constr:
+        for index in constrIndices:
+            system.setParticleMass(index, 0.0 * unit.amu)
     else:
-        top, pos = ligand_top, ligand_pos
-        ff = app.ForceField()
+        # Use restrained, not constrained
+        force_constant = restr * unit.kilocalories_per_mole/unit.angstroms**2
+        restraint_force = mm.CustomExternalForce('(k/2)*((x-x0)^2 + (y-y0)^2 + (z-z0)^2)')
+        restraint_force.addGlobalParameter('k', force_constant)
+        restraint_force.addPerParticleParameter('x0')
+        restraint_force.addPerParticleParameter('y0')
+        restraint_force.addPerParticleParameter('z0')
+        for index in constrIndices:
+            restraint_force.addParticle(index, coord_map[index] / 10)
+        system.addForce(restraint_force)
+
+    context = mm.Context(
+        system, 
+        mm.LangevinIntegrator(300*unit.kelvin, 1/unit.picoseconds, 2*unit.femtoseconds)
+    )
+
+    energies = []
+    for ci in range(mol.GetNumConformers()):
+        pos = mol.GetConformer(ci).GetPositions() / 10
+        for index in coord_map:
+            pos[index] = coord_map[index] / 10
+        context.setPositions(pos)
+        mm.LocalEnergyMinimizer.minimize(context)
+        state = context.getState(getPositions=True, getEnergy=True)
+        min_positions = state.getPositions(asNumpy=True)._value
+        energy = float(state.getPotentialEnergy()._value)
+        energies.append(energy)
+        for i, vec in enumerate(min_positions):
+            mol.GetConformer(ci).SetAtomPosition(i, [float(x) * 10 for x in vec])
     
-    # Create system with ff14SB and openff
+    # no protein, return
+    if not protein_pdb:
+        return mol, energies
+    
+    # minimization with protein
+    pdb = app.PDBFile(str(protein_pdb))
+    modeller = app.Modeller(pdb.topology, pdb.positions)
+    ligand_pos = mol.GetConformer(0).GetPositions() / 10
+    modeller.add(ligand_top, ndarray2quantity(ligand_pos))
+    top, pos = modeller.getTopology(), modeller.getPositions()
+    ff = app.ForceField('amber14/protein.ff14SB.xml', 'amber14/tip3p.xml')
+    pos = np.array([[vec.x, vec.y, vec.z] for vec in pos])
     ff.registerTemplateGenerator(generator)
     system = ff.createSystem(
         top, 
@@ -155,21 +197,21 @@ def constrained_optimization(mol: Chem.Mol, protein_pdb: os.PathLike, coord_map:
         mm.LangevinIntegrator(300*unit.kelvin, 1/unit.picoseconds, 2*unit.femtoseconds)
     )
 
-    # Constrained all protein atoms
+    constrIndices = []
+    # protein heavy atoms
     residues = list(top.residues())
     for residue in residues[:-1]:
         for atom in residue.atoms():
-            system.setParticleMass(atom.index, 0.0 * unit.amu)
-    
-    # Find constrained indices on ligands
-    constr_indices = []
+            if atom.element.symbol != 'H':
+                constrIndices.append(atom.index)
+    # ligand heavt atoms
     ligand_residue = list(top.residues())[-1]
-    for atom in ligand_residue.atoms():
-        if atom.index in coord_map:
-            constr_indices.append(atom.index)
+    for i, atom in enumerate(ligand_residue.atoms()):
+        if i in coord_map and atom.element.symbol != 'H':
+            constrIndices.append(atom.index)
 
     if constr:
-        for index in constr_indices:
+        for index in constrIndices:
             system.setParticleMass(index, 0.0 * unit.amu)
     else:
         # Use restrained, not constrained
@@ -179,16 +221,13 @@ def constrained_optimization(mol: Chem.Mol, protein_pdb: os.PathLike, coord_map:
         restraint_force.addPerParticleParameter('x0')
         restraint_force.addPerParticleParameter('y0')
         restraint_force.addPerParticleParameter('z0')
-        for index in constr_indices:
-            restraint_force.addParticle(index, coord_map[index] / 10)
+        for index in constrIndices:
+            restraint_force.addParticle(index, pos[index])
         system.addForce(restraint_force)
 
     energies = []
     for ci in range(mol.GetNumConformers()):
         pos[-num_ligand_atoms:] = mol.GetConformer(ci).GetPositions() / 10
-        if constr:
-            for index in constr_indices:
-                pos[index] = coord_map[index] / 10
         context.setPositions(pos)
         mm.LocalEnergyMinimizer.minimize(context)
         state = context.getState(getPositions=True, getEnergy=True)
