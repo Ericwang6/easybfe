7706b64de647f143757347865e6af5d38c2eafe4
Yingze Wang <ericwangyz@berkeley.edu>
2026-01-05 22:39:07 -0800

    add logging.py

---
+++ easybfe/logging.py
@@ -0,0 +1,28 @@
+import logging
+import os
+from logging.handlers import RotatingFileHandler
+
+
+def setup_logging(log_file: os.PathLike = '', level = logging.INFO) -> logging.Logger:
+    # logging
+    logger = logging.getLogger('easybfe')
+    logger.propagate = False
+    logger.setLevel(level)
+    formatter = logging.Formatter(
+        "%(asctime)s [%(levelname)s] %(process)d %(name)s: %(message)s"
+    )
+
+    # file
+    if log_file is not None:
+        handler = RotatingFileHandler(str(log_file), maxBytes=50 * 1024 * 1024, backupCount=5)
+        handler.setLevel(logging.INFO)
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    # console
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    console.setFormatter(formatter)
+    logger.addHandler(console)
+
+    return logger
