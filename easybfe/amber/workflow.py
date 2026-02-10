f023d752e4b201b33952f08e4e032b7bf77b7b66
Ericwang6 <ericwangyz@berkeley.edu>
2026-02-10 01:47:17 -0800

    fix echo

--- easybfe/amber/workflow.py
+++ easybfe/amber/workflow.py
@@ -218,7 +218,7 @@ run_step_seq() {
     for name in step_names:
         cfg = workflows[0].steps[name].config
         pmemd_exec = cfg.exec if cfg.exec.endswith('.MPI') else f'{cfg.exec}.MPI'
-        script_lines.append(f"\nRunning {name}")
+        script_lines.append(f'echo "Running {name}"')
         if cfg.use_mpi:
             create_groupfile_from_steps([wf.steps[name] for wf in workflows], wdir, wdir / f'{name}.groupfile')
             cmd = f"mpirun -np {nprocs} {pmemd_exec} -ng {len(workflows)} -groupfile {name}.groupfile"
