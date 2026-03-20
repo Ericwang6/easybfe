42e4fd1045ed13a5cf337779db8f179998137a2a
Ericwang6 <ericwangyz@berkeley.edu>
2026-03-20 02:10:03 -0700

    debug mode in examples

--- examples/rbfe/script.sh
+++ examples/rbfe/script.sh
@@ -1,2 +1,2 @@
-easybfe ligand pargen ../tyk2/tyk2_ligands.sdf -f gaff2 -c gas -o ./ligands
-easybfe rbfe setup config_rbfe_5ns.json
+easybfe ligand pargen ../tyk2/tyk2_ligands.sdf -f gaff2 -c gas -O ./ligands
+easybfe rbfe setup config_rbfe_debug.json
